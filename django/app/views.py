3e1ba807f9b351ae9c1d4f97e2fc58a249b06378
sudharsanboomika <sudharsanboomika@localhost>
2025-12-28 19:52:18 +0530

    code change

---
+++ django/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    """Basic home page view"""
+    return HttpResponse("Hello, World! Welcome to Django!")
+
+def about(request):
+    """Basic about page view"""
+    return HttpResponse("This is the about page of our Django application.")
+
+def home_template(request):
+    """Home page view using a template"""
+    return render(request, 'app/home.html')
