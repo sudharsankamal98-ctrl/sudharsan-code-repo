caa2ec710b298013de0ddac0accb3a8f17ab47c0
sudharsanboomika <sudharsanboomika@localhost>
2026-01-07 11:29:50 +0530

    common template and static file

--- django/projectName/settings.py
+++ django/projectName/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'projectName.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'app' / 'static',
+# ]
+
+
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'app' / 'static',
+    BASE_DIR / "static",  # Ensure your static files folder is set
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
